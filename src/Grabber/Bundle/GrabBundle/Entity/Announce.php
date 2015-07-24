6496092aceda9b54853dbfb5889bdc09ddb717af
Rodion Smakota <rsmakota@gmail.com>
2015-07-24 18:39:41 +0300

    two new object

--- src/Grabber/Bundle/GrabBundle/Entity/Announce.php
+++ src/Grabber/Bundle/GrabBundle/Entity/Announce.php
@@ -1,17 +1,16 @@
 <?php
 /**
- * @author    Rodion Smakota <rsmakota@gmail.com>
- * @copyright 2015 Grabber LLC
+ * @author    Rodion Smakota <rsmakota@nebupay.com>
+ * @copyright 2015 Nebupay LLC
  */
+
 namespace Grabber\Bundle\GrabBundle\Entity;
+
 use \Doctrine\ORM\Mapping as Orm;
 
 /**
- * Class Category
- *
- * @package Grabber\Bundle\GrabBundle\Entity
  * @ORM\Entity
- * @ORM\Table(name="announces")
+ * @ORM\Table(name="announce")
  */
 class Announce
 {
@@ -23,121 +22,19 @@ class Announce
     private $id;
 
     /**
-     * @ORM\ManyToOne(targetEntity="Grabber\Bundle\GrabBundle\Entity\Source")
-     */
-    private $source;
-    /**
-     * @ORM\ManyToOne(targetEntity="Grabber\Bundle\GrabBundle\Entity\Category")
-     */
-    private $category;
-
-    /**
-     * ID on the resource
-     * @ORM\Column(type = "integer")
-     */
-    private $index;
-
-    /**
-     * @ORM\Column(type = "string")
+     * @ORM\Column(type='string')
      */
     private $uri;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\ManyToOne(targetEntity="Grabber\Bundle\GrabBundle\Entity\Region")
      */
-    private $created;
-
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    public function setCreated($created)
-    {
-        $this->created = $created;
-    }
-
-    public function getCategory()
-    {
-        return $this->category;
-    }
-
-    public function setCategory($category)
-    {
-        $this->category = $category;
-    }
+    private $region;
 
     /**
-     * @return string
+     * @ORM\Column(type='boolean')
      */
-    public function getUri()
-    {
-        return $this->uri;
-    }
-
-    /**
-     * @param string $uri
-     */
-    public function setUri($uri)
-    {
-        $this->uri = $uri;
-    }
+    private $processed;
 
 
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSource()
-    {
-        return $this->source;
-    }
-
-    /**
-     * @param mixed $source
-     */
-    public function setSource($source)
-    {
-        $this->source = $source;
-    }
-
-
-    /**
-     * @return mixed
-     */
-    public function getIndex()
-    {
-        return $this->index;
-    }
-
-    /**
-     * @param mixed $index
-     */
-    public function setIndex($index)
-    {
-        $this->index = $index;
-    }
-
-    /**
-     * @return string
-     */
-    public static function clazz()
-    {
-        return get_class();
-    }
 }
